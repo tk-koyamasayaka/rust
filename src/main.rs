54215e8fb8852dfcdc48747553ae6c4282e049c5
Sayaka Koyama <61900280+wannyanland@users.noreply.github.com>
2022-06-25 19:13:23 +0900

    数当てゲーム 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng; // 乱数生成のためのメソッドを定義
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    /*
+     * thread_rng -> Rngトレイトの中で定義されている
+     * 1..101 -> 開始..終了(1 <= x < 101 の意味)
+     * 1.=100 これでもいい
+     */
+    let secret_number = rand::thread_rng().gen_range(1..101); 
+
+    println!("秘密の数字は次の通りだよ {} ", secret_number);
+
+    println!("数字をに入力してね");
 
     let mut guess = String::new();
 
@@ -11,5 +22,14 @@ fn main() {
     .read_line(&mut guess)
     .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let guess: u32 = guess.trim().parse()
+    .expect("数字を入力してください");
+
+    println!("あなたが決めた数字 {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
