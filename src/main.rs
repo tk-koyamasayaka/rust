9caddfbb8ec90790afa21f104409d2e63cb9afcf
Sayaka Koyama <61900280+wannyanland@users.noreply.github.com>
2022-07-11 23:53:42 +0900

    構造体でリファクタリングする: より意味付けする

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 // 構造体を定義し、インスタンス化する
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 fn main() {
-    let lect = (30, 50);
+    let lect = Rectangle { width: 30, height: 50};
 
     println!(
         // 長方形の面積は、{}平方ピクセルです
         "The area of the rectangle is {} square pixels.",
-        area(lect)
+        area(&lect)
     );
 }
 
-fn area(dimentions: (u32, u32)) -> u32 {
-    dimentions.0 * dimentions.1
+// 構造体の所有権を奪うよりも借用する必要があります。こうすることでmainは所有権を保って、
+// rect1を使用し続けることができ、そのために関数シグニチャと関数呼び出し時に&を使っている
+fn area(dimentions: &Rectangle) -> u32 {
+    dimentions.width * dimentions.height
 }
