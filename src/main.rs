5bba8185b311125664d4b4a0d129a375ff021e79
Sayaka Koyama <61900280+wannyanland@users.noreply.github.com>
2022-07-12 00:00:39 +0900

    トレイトの導出で有用な機能を追加する

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
 // 構造体を定義し、インスタンス化する
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-    let lect = Rectangle { width: 30, height: 50};
+    let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!(
-        // 長方形の面積は、{}平方ピクセルです
-        "The area of the rectangle is {} square pixels.",
-        area(&lect)
-    );
-}
-
-// 構造体の所有権を奪うよりも借用する必要があります。こうすることでmainは所有権を保って、
-// rect1を使用し続けることができ、そのために関数シグニチャと関数呼び出し時に&を使っている
-fn area(dimentions: &Rectangle) -> u32 {
-    dimentions.width * dimentions.height
+    println!("rect1 is {:#?}", rect1);
 }
