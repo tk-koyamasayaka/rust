869871683f4919b2282167ba587c596828bcac51
Sayaka Koyama <61900280+wannyanland@users.noreply.github.com>
2022-07-11 23:48:50 +0900

    タプルでリファクタリングする

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 // 構造体を定義し、インスタンス化する
 
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let lect = (30, 50);
 
     println!(
         // 長方形の面積は、{}平方ピクセルです
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(lect)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimentions: (u32, u32)) -> u32 {
+    dimentions.0 * dimentions.1
 }
-
-
-// TODO タプルでリファクタリングする
