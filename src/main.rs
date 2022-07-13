c312404cfdea5355cea4c7eff5f262f658742f75
Sayaka Koyama <61900280+wannyanland@users.noreply.github.com>
2022-07-13 23:56:14 +0900

    メソッド記法 メソッドを定義する

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,15 @@ struct Rectangle {
     height: u32,
 }
 
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!("rect1 is {:#?}", rect1);
+    println!("rect1 is {:#?}", rect1.area());
 }
